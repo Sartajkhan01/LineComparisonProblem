237da7b3731dcd3fb6ecfa8df00466612a27728a
Sartajkhan01 <106169841+Sartajkhan01@users.noreply.github.com>
2022-06-09 23:29:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Day_3_Assignment
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Two Line Comparison");
+
+            UC1_CalculateLength abc = new UC1_CalculateLength();
+            abc.Line();
+
+
+        }
+
+    }
+}
+ 
+   
+   
+
+
+
+
