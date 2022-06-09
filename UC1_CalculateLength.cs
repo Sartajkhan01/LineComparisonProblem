82a57ce459013d0d47c81e8e0c98b9ef49d85f7d
Sartajkhan01 <106169841+Sartajkhan01@users.noreply.github.com>
2022-06-09 23:25:37 +0530

    Add files via upload

---
+++ UC1_CalculateLength.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3_Assignment
+{
+    internal class UC1_CalculateLength
+    {
+        public void Line()
+        {
+            Console.WriteLine("Enter the Value for X1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Value for X2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Value for Y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Value for Y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double result = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine(result);
+        }
+    }
+}
