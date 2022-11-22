af7849954914a97ac71ef57620532216a669c6b2
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2022-11-22 22:14:48 +0530

    Add project files.

---
+++ LineComparisonProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LineComparisonProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Line Comparison Program.....");
+            LineComparison length = new LineComparison(2, 5, 4, 9);
+            length.CalculatingLength();
+        }
+    }
+}
