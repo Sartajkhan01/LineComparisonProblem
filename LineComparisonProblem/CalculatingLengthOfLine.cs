2da68940c164168258812705f908f7e3f7ef121d
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-02 21:44:32 +0530

    [Sartaj]: added Comparison Of Two Lines

--- LineComparisonProblem/CalculatingLengthOfLine.cs
+++ LineComparisonProblem/CalculatingLengthOfLine.cs
@@ -8,19 +8,29 @@ namespace LineComparisonProblem
 {
     public class LineComparison
     {
-        int x1, x2, y1, y2;
-        public LineComparison(int x1, int x2, int y1, int y2)
+
+        int X1;
+        int Y1;
+        int X2;
+        int Y2;
+        double Length_XY;
+        public void CalculateLength()
         {
-            this.x1 = x1;
-            this.x2 = x2;
-            this.y1 = y1;
-            this.y2 = y2;
-        }
-        public double CalculatingLength()
-        {
-            double result = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine("Length Of The Line is {0} :", result);
-            return result;
+            Console.WriteLine("Enter X,Y Co_Ordinates of the Point 1: ");
+            Console.Write("Enter Value X1: ");
+            X1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Value Y1: ");
+            Y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter X,Y Co_Ordinates of the Poin 2: ");
+            Console.Write("Enter Value X2: ");
+            X2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Value Y2: ");
+            Y2 = Convert.ToInt32(Console.ReadLine());
+            Length_XY = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+            Console.WriteLine("Length of Value is : " + Length_XY);
+            Console.WriteLine("==================================================================================");
+
+
         }
     }
 }
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -1,34 +1,14 @@
 ﻿namespace LineComparisonProblem
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To The Line Comparison Program.....");
-            //LineComparison length = new LineComparison(2,5,4,9);
-            //length.CalculatingLength();
-
-            LineComparison equating = new LineComparison(1, 4, 2, 8);
-            double line1 = equating.CalculatingLength();
-            LineComparison equating2 = new LineComparison(2, 5, 3, 8);
-            double line2 = equating2.CalculatingLength();
-
-            if (line1.CompareTo(line2) == 0)
-            {
-                Console.WriteLine("Both the Lines are equal");
-            }
-            else
-            {
-                Console.WriteLine("Both the Lines are not equal");
-            }
-            if (line1.CompareTo(line2) > 0)
-            {
-                Console.WriteLine("Line 1 is greater than Line 2");
-            }
-            else
-            {
-                Console.WriteLine("Line 2 is greater than Line1");
-            }
+            Console.WriteLine("==========Welcome To Line Comparison Computation Program==========");
+            LineComparison length = new LineComparison();
+            length.CalculateLength(); //Call The Methods
+            Console.ReadLine();
         }
     }
 }
