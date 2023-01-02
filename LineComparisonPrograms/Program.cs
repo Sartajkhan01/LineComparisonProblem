111c35c17f8c7b184bf66166c5564ce9c14e3709
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-02 22:30:43 +0530

    [Sartaj]: added Checking Equality of Lines

--- LineComparisonPrograms/Program.cs
+++ LineComparisonPrograms/Program.cs
@@ -9,6 +9,7 @@
             Console.WriteLine("==========Welcome To Line Comparison Computation Program==========");
             LineLength length = new LineLength();
             length.CalculateLength(); //Call The Methods
+            length.CheckEquality();
             Console.ReadLine();
         }
     }
