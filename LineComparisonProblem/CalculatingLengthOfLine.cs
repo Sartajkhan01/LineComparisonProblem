1189161de5b4735e82b18e3b66eb645c153c7665
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-09 13:25:17 +0530

    [Sartaj] : Added Comparison Problem Using Oops

--- LineComparisonProblem/CalculatingLengthOfLine.cs
+++ LineComparisonProblem/CalculatingLengthOfLine.cs
@@ -8,29 +8,54 @@ namespace LineComparisonProblem
 {
     public class LineComparison
     {
-
-        int X1;
-        int Y1;
-        int X2;
-        int Y2;
-        double Length_XY;
         public void CalculateLength()
         {
-            Console.WriteLine("Enter X,Y Co_Ordinates of the Point 1: ");
-            Console.Write("Enter Value X1: ");
-            X1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Value Y1: ");
-            Y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter X,Y Co_Ordinates of the Poin 2: ");
-            Console.Write("Enter Value X2: ");
-            X2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Value Y2: ");
-            Y2 = Convert.ToInt32(Console.ReadLine());
-            Length_XY = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
-            Console.WriteLine("Length of Value is : " + Length_XY);
-            Console.WriteLine("==================================================================================");
+            Console.WriteLine("----------Compuatation for first line----------");
+            int x1, y1, x2, y2;
 
+            Console.WriteLine("Enter the value of x1 :");
+            x1 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Enter the value of y1 :");
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of x2 :");
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of y2 :");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            var line1 = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+
+            Console.WriteLine("line1 :" + line1);
+
+            Console.WriteLine("--------Computation for second line---------");
+            int x3, y3, x4, y4;
+
+            Console.WriteLine("Enter the vaule of x3 :");
+            x3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of y3 :");
+            y3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of x4 :");
+            x4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of y4 :");
+            y4 = Convert.ToInt32(Console.ReadLine());
+
+            var line2 = Math.Sqrt((Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2)));
+
+            Console.WriteLine("line2 :" + line2);
+
+            Console.WriteLine("----------ComparionTo Method----------");
+
+            Console.WriteLine("Line 1 = " + line1);
+            Console.WriteLine("Line 2 = " + line2);
+            Console.WriteLine("Return value (comparison) = " + line1.CompareTo(line2));
         }
+       
+
+        
     }
 }
